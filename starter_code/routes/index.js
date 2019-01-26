240cac4e51c5744e5d5cccc5b545388d00f824b6
guilhermeaeme <guilhermeaeme@gmail.com>
2019-01-25 23:04:31 -0200

    started

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router  = express.Router();
 
+const Movie = require('../models/movie.js');
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+/* GET movies */
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      console.log(movies)
+
+      res.render("movies", {movies});
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+/* GET movie */
+router.get('/movie/:id', (req, res, next) => {
+  let movieId = req.params.id;
+  Movie.findOne({'_id': movieId})
+    .then(movie => {
+      res.render("movie-detail", { movie })
+    })
+    .catch(error => {
+      console.log(error)
+    })
 });
 
 module.exports = router;
